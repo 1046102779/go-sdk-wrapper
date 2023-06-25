d7564c128dfc037e6515bc846fd48db8040cf227
seachen <seachen@tencent.com>
2023-06-25 10:58:12 +0800

    optimize: 支持PB对枚举值序列化的支持
    Signed-off-by: seachen <seachen@tencent.com>

--- wrapper.go
+++ wrapper.go
@@ -28,7 +28,7 @@ func newAdapterDaprAPI(log *logger.Logger) *adapterDaprAPI {
 	return &adapterDaprAPI{
 		log:          log,
 		decodeParser: protojson.UnmarshalOptions{DiscardUnknown: true},
-		encodeParser: protojson.MarshalOptions{},
+		encodeParser: protojson.MarshalOptions{UseEnumNumbers: true},
 		pbType:       reflect.TypeOf((*proto.Message)(nil)).Elem(),
 	}
 }
